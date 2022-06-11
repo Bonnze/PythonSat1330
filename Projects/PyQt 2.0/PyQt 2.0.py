e3a059355d020807d6c54def4b09ee1154cb2ff3
Bonnze <bonnzebrian@gmail.com>
2022-06-11 12:04:22 +0100

    Update PyQt 2.0.py

--- Projects/PyQt 2.0/PyQt 2.0.py
+++ Projects/PyQt 2.0/PyQt 2.0.py
@@ -1,3 +1,20 @@
 #pyqt 1.0 
 # https://docs.google.com/presentation/d/1yKphcke3fU9ngtN5ob5EMkL-21DzqxBD5uNaZNJwhWo/edit?usp=sharing
 #calculator
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+
+class Mywindow(QMainWindow):
+    def init(self):
+        super(Mywindow,self).init()
+        self.counter = 0
+        self.setGeometry(200,200,300,300)
+        self.setWindowTitle("Calculator")
+
+
+        
+app = QApplication(sys.argv)
+window  = Mywindow()
+window.show()
+sys.exit(app.exec_())
